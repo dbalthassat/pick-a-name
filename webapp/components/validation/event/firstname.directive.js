6ebbdcf72de6d0726be731b5a6c45594365e843a
Damien Balthassat <balthassat.damien@gmail.com>
2015-11-19 19:59:26 +0100

    Update angular controls on event participation.

--- webapp/components/validation/event/firstname.directive.js
+++ webapp/components/validation/event/firstname.directive.js
@@ -1,35 +1,36 @@
-export function ValidationEventFirstnameDirective($q, $timeout, Restangular) {
+export function ValidationEventFirstnameDirective($q, Restangular) {
 	'ngInject';
 
 	return {
 		restrict: 'A',
 		require: 'ngModel',
 		link: function(scope, elem, attr, model) {
-			var _event;
+			var _person;
 			var previousValidator;
 
 			model.$asyncValidators['exists-in-event'] = function(modelValue, viewValue) {
 				previousValidator = null;
-				_event = null;
+				_person = null;
 				var def = $q.defer();
 				if(!viewValue) {
 					def.resolve();
 				} else {
-					var restEvent = Restangular.all('events/1/persons');
+					var restEvent = Restangular.all('events/' + attr.validationEventFirstname + '/persons');
 					var promise = restEvent.get(viewValue);
 					promise.then(function(event) {
 						if(!event) {
 							def.reject();
 						} else {
-							_event = event;
+							_person = event.person;
 							def.resolve();
+
 						}
 					}, function() {
 						def.reject();
 					});
 				}
 				previousValidator = def.promise;
-				return previousValidator;
+				return def.promise;
 			};
 
 			model.$asyncValidators['already-has-friend'] = function(modelValue) {
@@ -38,14 +39,15 @@ export function ValidationEventFirstnameDirective($q, $timeout, Restangular) {
 					def.resolve();
 				} else {
 					previousValidator.then(function() {
-						if(_event && _event.hasFriend || modelValue !== _event.name) {
+						if(_person && _person.hasFriend || modelValue !== _person.name) {
 							def.reject();
 						} else {
 							def.resolve();
 						}
+					}, function() {
+						def.resolve();
 					});
 				}
-
 				return def.promise;
 			};
 		}
